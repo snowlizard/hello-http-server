66ef7c41b80df81d5c42fc6475189ff41534a671
julian <illegal_burrito@live.com>
2022-03-10 16:34:44 -0800

    first node project done

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // write your code here
+const http = require('http');
+
+const server = http.createServer( (req, res) => {
+    console.log(`Recieved ${req.method} request for ${req.url}`);
+
+    res.writeHead(200, {'content-type': 'text/plain'});
+    res.end('Hello World');
+});
+
+server.listen(8080, 'localhost', null, () => {
+    console.log('server is listening on localhost:8080');
+})
